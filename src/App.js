90c00ff21d899bd7d4af62cb5234c38634cc4ae5
O-tega <tega.myview@gmail.com>
2022-05-31 17:07:32 +0100

    Accordion creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+const items = [
+	{ title: "What is React", content: "React is a front-end framework" },
+	{ title: "Why use React?", content: "React is a favourite js library amongst engineers" },
+	{ title: "How do you use React?", content: "You use react by creating Components" },
+];
+
+const App = () => {
+	return (
+		<div>
+			<h1 className="text-2xl font-medium text-blue-500 text-center">React App for Accordion</h1>
+			<div className="flex justify-center pt-10 w-full">
+				<Accordion item = {items}/>
+				</div>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { Collapse } from "antd";
+import { CaretDownOutlined } from "@ant-design/icons";
+
+const { Panel } = Collapse;
+
+const Accordion = ({ item }) => {
+	const [activeIndex, setActiveIndex] =
+		useState(null);
+
+	const clicked = (index) => {
+		setActiveIndex(index);
+	};
+
+	const renderItems = item.map(
+		(items, index) => {
+			const display =
+				index === activeIndex
+					? ""
+					: "hidden";
+			const rotate =
+				index === activeIndex
+					? ""
+					: -90;
+			return (
+				<div
+					key={index}
+					className='border transition-all duration-700'
+					onClick={() => clicked(index)}
+				>
+					<div className='p-3 '>
+						<div>
+							<h1 className='font-medium text-blue-500'>
+								<span>
+									{" "}
+									<CaretDownOutlined
+										rotate={rotate}
+									/>{" "}
+								</span>
+								{items.title}
+							</h1>
+						</div>
+						<div
+							className={`${display} `}
+						>
+							<p className='pl-5 pt-5'>
+								{items.content}
+							</p>
+						</div>
+					</div>
+				</div>
+			);
+		}
+	);
+	return (
+		<div className='w-9/12'>
+			{renderItems}
+		</div>
+	);
+};
+
+export default Accordion;
